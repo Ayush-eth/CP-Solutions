75b544780eb04bd4b784a219a22900d531422439
Ayush <91387353+Ayush-eth@users.noreply.github.com>
2022-08-19 15:10:30 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1473B | String LCM/156947557.cpp
@@ -0,0 +1,82 @@
+/**
+ *    author:  _Adam
+ *    created: 04.04.2022 1:07:15       
+**/
+ 
+ 
+#undef _GLIBCXX_DEBUG
+ 
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define vi vector<int>
+#define vs vector<string>
+#define vll vector<long long>
+#define pb push_back
+#define vii vector<int64_t>
+#define vc vector<char>
+#define si set<int>
+#define mii map<int, int>
+#define pqMax priority_queue<int>
+#define pqMin priority_queue<int, vector<int>, greater<int>>
+#define read(x) ll t;cin>>t
+#define sz(V) ll(V.size())
+ 
+template<typename T>
+ll bs(T arr[], T l, T r, T x){
+    if (r >= l) {
+        ll mid = l + (r - l) / 2;
+        if (arr[mid] == x)
+            return 1;
+        if (arr[mid] > x)
+            return bs(arr, l, mid - 1, x);
+        return bs(arr, mid + 1, r, x);
+    }
+    return 0;
+}
+ 
+ 
+//Solution start here
+void solve() {
+    string str1,str2;cin>>str1>>str2;
+    ll n1 = str1.length();
+    ll n2 = str2.length();
+    ll lcm = n1*n2/__gcd(n1,n2);
+    string r1 = "",r2="";
+    for(ll i=0;i<lcm/n2;i++){
+      r1+=str2;
+    }
+    for(ll i=0;i<lcm/n1;i++){
+      r2+=str1;
+    }
+    if(r1==r2){
+      cout<<r1<<endl;
+    }
+    else cout<<-1<<endl;
+    
+    
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+    #endif
+ 
+    read(x);
+    while(t--) {
+        solve();
+    }
+ 
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
