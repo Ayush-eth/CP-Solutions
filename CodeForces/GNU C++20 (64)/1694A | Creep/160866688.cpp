6d16916350a5ac2d7f7b9d93d6565694d1ccb06e
Ayush <91387353+Ayush-eth@users.noreply.github.com>
2022-08-19 15:06:29 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1694A | Creep/160866688.cpp
@@ -0,0 +1,84 @@
+/**
+ *    author:  _Adam
+ *    created: 04.04.2022 1:07:15       
+**/
+ 
+ 
+#undef _GLIBCXX_DEBUG
+ 
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define vi vector<int>
+#define vs vector<string>
+#define vll vector<long long>
+#define pb push_back
+#define vii vector<int64_t>
+#define vc vector<char>
+#define si set<int>
+#define mii map<int, int>
+#define pqMax priority_queue<int>
+#define pqMin priority_queue<int, vector<int>, greater<int>>
+#define read(x) ll t;cin>>t
+#define sz(V) ll(V.size())
+#define endl '\n'
+#define mod 1000000007
+ 
+template<typename T>
+ll bs(T arr[], T l, T r, T x){if (r >= l) {ll mid = l + (r - l) / 2;if (arr[mid] == x)return 1;if (arr[mid] > x)return bs(arr, l, mid - 1, x);return bs(arr, mid + 1, r, x);}return 0;}
+ll fibo(ll n){return int((pow((1+sqrt(5))/2,n)-pow((1-sqrt(5))/2,n))/sqrt(5));}
+ 
+/*----------------------------------------------------------------------------GOD SAYS---------------------------------------------------------------------------------*/
+ 
+ 
+void solve(){
+    ll z,o;
+    cin>>z>>o;
+    ll m=min(z,o);
+    for(ll i=1;i<=2*m;i++){
+     if(i%2==1){
+      cout<<1;
+     }
+     else cout<<0;
+    }
+    if((z-o)>0){
+     for(ll i=0;i<z-o;i++){
+      cout<<0;
+     }
+    }
+    else{
+     for(ll i=0;i<o-z;i++){
+      cout<<1;
+     }
+    }
+    cout<<endl;
+}
+ 
+    
+ 
+ 
+/*-------------------------------------------------------------------IT WAS GOD'S DECISION---------------------------------------------------------------------------------*/
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+    #endif
+ 
+    read(x);
+    while(t--) {
+        solve();
+    }
+ 
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
