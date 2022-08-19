781986b02d2093a0a4a51557b43fe339eedc9097
Ayush <91387353+Ayush-eth@users.noreply.github.com>
2022-08-19 15:12:40 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1665A | GCD vs LCM/153037755.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define pb push_back
+ 
+int main(){
+    ll t; in t;
+    while(t--){
+        ll n; in n;
+        if (n%4==0) out n/4<<" "<<n/4<<" "<<n/4<<" "<<n/4<<endl;
+        else out n-3<<" "<<1<<" "<<1<<" "<<1<<endl;
+ 
+    }
+}
