f7b0d5c8c0e06b20d7573c1b3a0ba3c2cba84c37
Ayush <91387353+Ayush-eth@users.noreply.github.com>
2022-08-19 15:11:41 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1521A | Nastia and Nearly Good Numbers/155316194.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+ 
+int main(){
+    ll t;cin>>t;
+    while(t--){
+        ll a,b;cin>>a>>b;
+        if(b==1){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+            cout<<a<<" "<<a*b<<" "<<a*(b+1)<<endl;
+        }
+    }
+}
