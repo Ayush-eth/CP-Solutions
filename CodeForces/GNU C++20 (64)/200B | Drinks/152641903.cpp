7195890344dba4a8f5917d0a42167a728c3352c5
Ayush <91387353+Ayush-eth@users.noreply.github.com>
2022-08-19 15:13:15 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/200B | Drinks/152641903.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define for(n) for(ll i=0;i<n;i++)
+#define pb push_back
+ 
+ 
+int main(){
+    ll n;in n;
+    vector<ll>arr;
+    for(n){
+        ll temp; in temp;
+        arr.pb(temp);
+    }
+ 
+    long double sum=0;
+    for(n){
+        sum= sum+arr[i];
+    }
+    long double a = sum/n;
+    printf("%0.12Lf",a);
+    // out setprecision(14)<<a<<endl;
+ 
+}
